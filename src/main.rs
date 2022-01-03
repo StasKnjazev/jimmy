7a68dc5dc6942e39bb02a74f08e777440ea18dc2
xylous <xylous.e@gmail.com>
2022-01-03 20:10:26 +0200

    Add function to read file & use serde-rs to parse

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::process::exit;
-
 use clap::{App, Arg};
+
+mod data;
+use data::*;
 
 /// Determine if the given path exists *and* is a file
 fn is_file(path: &str) -> bool
@@ -8,7 +10,13 @@ fn is_file(path: &str) -> bool
     std::path::Path::new(path).is_file()
 }
 
-fn main()
+/// Read a file's contents and return a String or, if reading failed, an Error.
+fn read_file(path: &str) -> Result<String, std::io::Error>
+{
+    std::fs::read_to_string(path)
+}
+
+fn main() -> Result<(), std::io::Error>
 {
     let cli_args = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
@@ -23,4 +31,8 @@ fn main()
         eprintln!("error: provided path is not a file");
         exit(1);
     }
+    let contents = read_file(path)?;
+    let _data: ParsedInstallOptions = serde_yaml::from_str(&contents).unwrap();
+
+    Ok(())
 }
